8331cdc7836ef47958508ec0bc43d86d9fa6202b
Leo <leo.m.rocha@gmail.com>
2017-01-06 11:44:36 +0100

    Drills for day 2

---
+++ day-002/build.sbt
@@ -0,0 +1,6 @@
+name := "recursion"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ day-002/src/main/scala/recursion.scala
@@ -0,0 +1,41 @@
+//package leo.training
+//Day 2
+//Recursion and High orderr Functions
+
+
+object main {
+
+  def main (args: Array[String]) = {
+    println("This is a test")
+  }
+
+  //Exercise 1
+  //Write a function for Factorial in an imperative way
+  //Write the test for it too under the src/test/scala directory
+  def factImp(n: Int): Int = {
+    ???
+  }
+
+  //Exercise 2.a
+  //Write a function for Factorial in a recursive way, try to make it tail recursive
+  //Write the test for it too under the src/test/scala directory
+  def factRec(n: Int): Int = {
+    ???
+  }
+
+
+  //Exercise 3
+  //Write a Fibonacci function in an imperative way
+  //The function must find the Nth Fibonacci Element
+  //Write the test for it too under the src/test/scala directory
+
+  def fibImp(n: Int): Int = {
+    ???
+  }
+  //Exercise 4
+  //Write a Fibonacci function in a recursive way, try to make it tail recursive
+  //Write the test for it too under the src/test/scala directory
+  def fibRec(n: Int): Int = {
+    ???
+  }
+}
