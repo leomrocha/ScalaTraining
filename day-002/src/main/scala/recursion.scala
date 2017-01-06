0d2fa6f635ee052ef2d111e754c227e760be2592
Leo <leo.m.rocha@gmail.com>
2017-01-06 14:26:56 +0100

    First examples on recursion and testing

--- day-002/src/main/scala/recursion.scala
+++ day-002/src/main/scala/recursion.scala
@@ -1,29 +1,61 @@
-//package leo.training
+package leo.training
 //Day 2
 //Recursion and High orderr Functions
 
 
-object main {
+object recursion {
 
-  def main (args: Array[String]) = {
+  def main (args: Array[String]): Unit = {
     println("This is a test")
+    println( 1 until 5) // 1,2,3,4
+    println (1 to 5 ) // 1 2 3 4 5
+    val v = factTailRec(6)
   }
 
   //Exercise 1
   //Write a function for Factorial in an imperative way
   //Write the test for it too under the src/test/scala directory
-  def factImp(n: Int): Int = {
-    ???
+  def factImp(n: Int ): Int = {
+    require (n>=0)
+    var fact = 1
+    for (i <- 1 to n){
+      fact = fact * i
+    }
+    fact
   }
 
   //Exercise 2.a
   //Write a function for Factorial in a recursive way, try to make it tail recursive
   //Write the test for it too under the src/test/scala directory
   def factRec(n: Int): Int = {
-    ???
+    //require(n>=0)
+    n match {
+      case x if x < 0 => 0
+      case 0 => 1
+      case x => x * factRec( x - 1)
+    }
   }
 
+  def factRec1(n: Int): Int = {
+    require(n >= 0)
+    if (n == 0) {
+      1
+    }
+    else {
+      n * factRec(n - 1)
+    }
+  }
 
+    def factTailRec(n: Int): Int = {
+      require(n>=0)
+      @annotation.tailrec
+      def go(n: Int, acc:Int): Int =
+        n match {
+          case 0 => 1
+          case x => go(x - 1, acc * x)
+        }
+      go(n, 1)
+    }
   //Exercise 3
   //Write a Fibonacci function in an imperative way
   //The function must find the Nth Fibonacci Element
---
+++ day-002/src/test/scala/recursionSpec.scala
@@ -0,0 +1,99 @@
+import leo.training._
+
+import org.scalatest.FlatSpec
+
+class RecursionSpec extends FlatSpec {
+
+  "factImp" should "be 0" in {
+    assert(recursion.factImp(0) == 1)
+  }
+
+  "factImp" should "be 1" in {
+    assert(recursion.factImp(1) == 1)
+  }
+  "factImp" should "be 2" in {
+    assert(recursion.factImp(2) == 2)
+  }
+  "factImp" should "be 6" in {
+    assert(recursion.factImp(3) == 6)
+  }
+  "factImp" should "be 24" in {
+    assert(recursion.factImp(4) == 24)
+  }
+  "factImp" should "be 120" in {
+    assert(recursion.factImp(5) == 120)
+  }
+  "factImp" should "be 720" in {
+    assert(recursion.factImp(6) == 720)
+  }
+  "factImp of a negative number" should "be refused" in {
+    try{
+      recursion.factImp(-1)
+    }
+    catch {
+      case _:IllegalArgumentException => //Expected
+    }
+    succeed
+  }
+
+  "factRec" should "be 0" in {
+    assert(recursion.factRec(0) == 1)
+  }
+  "factRec" should "be 1" in {
+    assert(recursion.factRec(1) == 1)
+  }
+  "factRec" should "be 2" in {
+    assert(recursion.factRec(2) == 2)
+  }
+  "factRec" should "be 6" in {
+    assert(recursion.factRec(3) == 6)
+  }
+  "factRec" should "be 24" in {
+    assert(recursion.factRec(4) == 24)
+  }
+  "factRec" should "be 120" in {
+    assert(recursion.factRec(5) == 120)
+  }
+  "factRec" should "be 720" in {
+    assert(recursion.factRec(6) == 720)
+  }
+  "factRec of a negative number" should "be refused" in {
+    try{
+      recursion.factRec(-1)
+    }
+    catch {
+      case _:IllegalArgumentException => //Expected
+    }
+    succeed
+  }
+  "factTailRec" should "be 0" in {
+    assert(recursion.factTailRec(0) == 1)
+  }
+  "factTailRec" should "be 1" in {
+    assert(recursion.factTailRec(1) == 1)
+  }
+  "factTailRec" should "be 2" in {
+    assert(recursion.factTailRec(2) == 2)
+  }
+  "factTailRec" should "be 6" in {
+    assert(recursion.factTailRec(3) == 6)
+  }
+  "factTailRec" should "be 24" in {
+    assert(recursion.factTailRec(4) == 24)
+  }
+  "factTailRec" should "be 120" in {
+    assert(recursion.factTailRec(5) == 120)
+  }
+  "factTailRec" should "be 720" in {
+    assert(recursion.factTailRec(6) == 720)
+  }
+  "factTailRec of a negative number" should "be refused" in {
+    try{
+      recursion.factTailRec(-1)
+    }
+    catch {
+      case _:IllegalArgumentException => //Expected
+    }
+    succeed
+  }
+}
